58d50f5d591e0a4dfc1a9d828721befd5997bd03
Alevtina Romanova <alevtinaromanova1606@yandex.ru>
2024-09-20 08:51:25 +0300

    Update main.py

--- PythonProjects/RequestsTest/main.py
+++ PythonProjects/RequestsTest/main.py
@@ -3,15 +3,7 @@ import requests
 URL = 'https://api.pokemonbattle.ru/v2'
 TOKEN = 'USER_TOKEN'
 HEADER = {'Content-Type':'application/json','trainer_token':TOKEN}
-body_registration= {
-    "trainer_token": TOKEN, 
-    "email": "USER_LOGIN",
-    "password": "USER_PASSWORD"
-}
 
-body_confirmation = {
-    "trainer_token":TOKEN
-}
 body_create = {
     
     "name": "Пеночка",
@@ -26,11 +18,6 @@ body_pokeboll = {
     
     "pokemon_id": "47868"
 }
-'''response = requests.post(url = f'{URL}/trainers/reg',headers = HEADER,json = body_registration)
-print(response.text)'''
-
-'''response_confirmation = requests.post(url = f'{URL}/trainers/confirm_email',headers = HEADER,json = body_confirmation)
-print(response_confirmation.text)'''
 
 '''response_create = requests.post(url = f'{URL}/pokemons',headers = HEADER,json = body_create)
 print(response_create.text)'''
