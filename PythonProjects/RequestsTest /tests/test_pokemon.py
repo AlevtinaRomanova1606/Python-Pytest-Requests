2b572cdc1f57f2057ff80e2ba8e550a9e05b1a8d
Alevtina Romanova <alevtinaromanova1606@yandex.ru>
2024-09-20 08:58:47 +0300

    Update test_pokemon.py

--- PythonProjects/RequestsTest /tests/test_pokemon.py
+++ PythonProjects/RequestsTest /tests/test_pokemon.py
@@ -12,3 +12,8 @@ def test_status_code():
 def test_part_of_response():
     response_get = requests.get(url= f'{URL}/trainers',params = {'trainer_id': TRAINER_ID})
     assert response_get.json()["data"][0]["trainer_name"] == 'LadyWaks'
+
+@pytest.mark.parametrize('key, value', [('trainer_name','LadyWaks'),('id',f'{TRAINER_ID}')])
+def test_parametrize(key,value):
+    response_parametrize = requests.get(url= f'{URL}/trainers',params = {'trainer_id': TRAINER_ID})
+    assert response_parametrize.json()["data"][0][key] == value
